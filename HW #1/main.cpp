b3decd8347bd60ebc2a64824ed7a5695ddddd2ef
YoelL <yoellev8@gmail.com>
2017-06-18 08:13:18 +0300

    Update main.cpp

--- HW #1/main.cpp
+++ HW #1/main.cpp
@@ -9,7 +9,8 @@
 // In each Header you will find a brief explenation of the Class perpuse.
 //functions that are highlited in * are used for debuging purpeses Only, and are not part of the Ex.
 // Logic of this Ex as folows
-// The Main calls to an Instance of Calculator which lunches the Menu, an instance of DB is created, and it initiate the DataBase, all of the arithmatic function are located in the DB.h , it seems that i have a problem with the power func and i didnt got it to work properly.
+// The Main calls to an Instance of Calculator which lunches the Menu, an instance of DB is created, 
+// and it initiate the DataBase, all of the arithmatic function are located in the DB.h.
 
 
 
